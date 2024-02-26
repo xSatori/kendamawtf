42dae7e478b9cba47743fe0e59bce4e6b9c52148
xSatori <99294685+xSatori@users.noreply.github.com>
2024-02-26 15:16:53 -0500

    Update VerticalFeatures.tsx

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -4,7 +4,7 @@ import { Section } from '../layout/Section';
 const VerticalFeatures = () => (
   <Section
     title="Ready to learn more about Kendama?"
-    description="Kendama is a traditional Japanese skill toy that consists of a handle (ken), a pair of cups (sarado), and a ball (tama) connected by a string. Its origins can be traced back several centuries, with roots in both Japanese and Western cultures. The modern kendama evolved from the classic Japanese game called "Hyakunin Isshu Karuta" and the French cup-and-ball game "bilboquet." Over time, kendama has evolved from a simple children's toy into a tool for art, competition, and mindfulness, attracting a global following. Today, it is not only a popular pastime but also a medium for educational programs, community-building activities, and a fantastic way to disconnect from your screens."
+    description="Kendama is a traditional Japanese skill toy."
   >
     <VerticalFeatureRow
       title="Your title here"
