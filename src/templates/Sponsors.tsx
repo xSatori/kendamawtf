8272c06a60434b6463fa35843dafd81dd9e8b59d
xSatori <99294685+xSatori@users.noreply.github.com>
2024-02-26 14:41:56 -0500

    Update Sponsors.tsx

--- src/templates/Sponsors.tsx
+++ src/templates/Sponsors.tsx
@@ -25,12 +25,12 @@ const Sponsors = () => (
           </td>
           <td className="border-2 border-gray-300 p-3">
             <a
-              href="https://nouns.wtf"
+              href="https://betterstack.com/?utm_source=github&utm_medium=sponsorship&utm_campaign=next-js-boilerplate"
               target="_blank"
             >
               <Image
-                src="/assets/images/nouns.png"
-                alt="Nouns"
+                src="/assets/images/better-stack-dark.png"
+                alt="Better Stack"
                 width={260}
                 height={224}
               />
