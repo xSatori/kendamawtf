eefc9aaeceebed3ccc265a032b0f8f51d8c1ddb5
xSatori <99294685+xSatori@users.noreply.github.com>
2024-02-26 14:45:21 -0500

    add skf

--- src/templates/Sponsors.tsx
+++ src/templates/Sponsors.tsx
@@ -5,11 +5,24 @@ import { Section } from '@/layout/Section';
 const Sponsors = () => (
   <Section
     title="Sponsors"
-    description="Our sponsors' exceptional support has made this project possible."
+    description="Made possible by these awesome organizations."
   >
     <table className="mx-auto border-collapse">
       <tbody>
         <tr className="h-56">
+          <td className="border-2 border-gray-300 p-3">
+            <a
+              href="https://sentry.io/for/nextjs/?utm_source=github&utm_medium=paid-community&utm_campaign=general-fy25q1-nextjs&utm_content=github-banner-nextjsboilerplate-logo"
+              target="_blank"
+            >
+              <Image
+                src="/assets/images/sentry-dark.png"
+                alt="Sentry"
+                width={260}
+                height={224}
+              />
+            </a>
+          </td>
           <td className="border-2 border-gray-300 p-3">
             <a
               href="https://sentry.io/for/nextjs/?utm_source=github&utm_medium=paid-community&utm_campaign=general-fy25q1-nextjs&utm_content=github-banner-nextjsboilerplate-logo"
