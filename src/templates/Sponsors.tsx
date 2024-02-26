d89845135e016da0411a553307409629d2b81860
xSatori <99294685+xSatori@users.noreply.github.com>
2024-02-26 14:38:45 -0500

    Update Sponsors.tsx

--- src/templates/Sponsors.tsx
+++ src/templates/Sponsors.tsx
@@ -12,12 +12,12 @@ const Sponsors = () => (
         <tr className="h-56">
           <td className="border-2 border-gray-300 p-3">
             <a
-              href="https://sentry.io/for/nextjs/?utm_source=github&utm_medium=paid-community&utm_campaign=general-fy25q1-nextjs&utm_content=github-banner-nextjsboilerplate-logo"
+              href="https://sweetskendamas.wtf"
               target="_blank"
             >
               <Image
                 src="/assets/images/sentry-dark.png"
-                alt="Sentry"
+                alt="Sweets Kendamas"
                 width={260}
                 height={224}
               />
@@ -25,12 +25,12 @@ const Sponsors = () => (
           </td>
           <td className="border-2 border-gray-300 p-3">
             <a
-              href="https://betterstack.com/?utm_source=github&utm_medium=sponsorship&utm_campaign=next-js-boilerplate"
+              href="https://nouns.wtf"
               target="_blank"
             >
               <Image
-                src="/assets/images/better-stack-dark.png"
-                alt="Better Stack"
+                src="/assets/images/nouns.jpg"
+                alt="Nouns DAO"
                 width={260}
                 height={224}
               />
