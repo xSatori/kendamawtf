ba00966dd80dad629093922250eecdb9949104e0
xSatori <99294685+xSatori@users.noreply.github.com>
2024-02-27 12:20:58 -0500

    Update Hero.tsx

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 import { Background } from '../background/Background';
 import { Button } from '../button/Button';
 import { HeroOneButton } from '../hero/HeroOneButton';
-import { Section } from '@/layout/Section';
-// import { Section } from '../layout/Section';
+// import { Section } from '@/layout/Section';
+import { Section } from '../layout/Section';
 // import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 // import { Logo } from './Logo';
 
