dfc68f5d42f4a6c604b9f1f5540ae37e2a6e2e1d
xSatori <99294685+xSatori@users.noreply.github.com>
2024-02-27 15:23:51 -0500

    Update Sponsors.tsx

--- src/templates/Sponsors.tsx
+++ src/templates/Sponsors.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React from 'react';
+
 import { Section } from '@/layout/Section';
 
 const Sponsors = () => (
@@ -7,38 +7,51 @@ const Sponsors = () => (
     title="Special Thanks"
     description="This was made possible thanks to these awesome organizations."
   >
-    <div className="flex justify-center items-center flex-wrap">
-      <div className="p-3 border-2 border-gray-300">
-        <a href="/" target="_blank" rel="noopener noreferrer">
-          <Image
-            src="/assets/images/SKF.png"
-            alt="Sweets Kendamas Foundation"
-            width={260}
-            height={224}
-          />
-        </a>
-      </div>
-      <div className="p-3 border-2 border-gray-300">
-        <a href="/" target="_blank" rel="noopener noreferrer">
-          <Image
-            src="/assets/images/noggles.png"
-            alt="Nouns"
-            width={260}
-            height={224}
-          />
-        </a>
-      </div>
-      <div className="p-3 border-2 border-gray-300">
-        <a href="/" target="_blank" rel="noopener noreferrer">
-          <Image
-            src="/assets/images/sweets.png"
-            alt="Sweets Kendamas"
-            width={260}
-            height={224}
-          />
-        </a>
-      </div>
-    </div>
+    <table className="mx-auto border-collapse">
+      <tbody>
+        <tr className="h-56">
+          <td className="border-2 border-gray-300 p-3">
+            <a
+              href="https://sentry.io/for/nextjs/?utm_source=github&utm_medium=paid-community&utm_campaign=general-fy25q1-nextjs&utm_content=github-banner-nextjsboilerplate-logo"
+              target="_blank"
+            >
+              <Image
+                src="/assets/images/SKF.png"
+                alt="Sweets Kendamas Foundation"
+                width={260}
+                height={224}
+              />
+            </a>
+          </td>
+          <td className="border-2 border-gray-300 p-3">
+            <a
+              href="https://sentry.io/for/nextjs/?utm_source=github&utm_medium=paid-community&utm_campaign=general-fy25q1-nextjs&utm_content=github-banner-nextjsboilerplate-logo"
+              target="_blank"
+            >
+              <Image
+                src="/assets/images/noggles.png"
+                alt="Nouns"
+                width={260}
+                height={224}
+              />
+            </a>
+          </td>
+          <td className="border-2 border-gray-300 p-3">
+            <a
+              href="https://betterstack.com/?utm_source=github&utm_medium=sponsorship&utm_campaign=next-js-boilerplate"
+              target="_blank"
+            >
+              <Image
+                src="/assets/images/sweets.png"
+                alt="Sweets Kendamas"
+                width={260}
+                height={224}
+              />
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </Section>
 );
 
