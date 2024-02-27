414a06233ec62e231cbab8d7e2d1c1b8cb81e738
xSatori <99294685+xSatori@users.noreply.github.com>
2024-02-27 13:34:47 -0500

    Update VerticalFeatures.tsx

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -3,20 +3,20 @@ import { Section } from '../layout/Section';
 
 const VerticalFeatures = () => (
   <Section
-    title="Welcome to the game of Kendama"
+    title="What exactly is kendama?"
     description="Kendama is a traditional Japanese skill toy."
   >
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/whatiskendama1.png"
-      imageAlt="What is Kendama?(1)"
+      title="Mindfulness Through Play"
+      description="The "Mindfulness Through Play" program by the Sweets Kendamas Foundation merges fun with mental well-being, using kendama to promote mindfulness and concentration. Participants engage in kendama's focused activity, finding a unique way to achieve a peaceful state of mind. This initiative not only enhances physical skills but also underscores mental health benefits, offering a novel approach to mindfulness. Through various activities, it encourages living in the moment, stress reduction, and joy in play."
+      image="/assets/images/SKF.png"
+      imageAlt="Sweets Kendamas Foundation"
     />
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/whatiskendama2.png"
-      imageAlt="What is Kendama?(2)"
+      title="Nouns DAO"
+      description="Nouns DAO is a groundbreaking decentralized autonomous organization exploring new frontiers in community engagement and project funding. While not directly related to kendama, the principles of creativity, community, and collaboration resonate with the kendama ethos. Nouns DAO's innovative approach to governance and project support offers intriguing possibilities for collaborations within the kendama community and beyond. Learn more about how Nouns DAO is shaping the future of collective action and creativity."
+      image="/assets/images/noggles.png"
+      imageAlt="Nouns DAO"
       reverse
     />
     <VerticalFeatureRow
