31f200dc8c904b07be64ca91e1efca3b5c7412e2
xSatori <99294685+xSatori@users.noreply.github.com>
2024-02-27 11:33:35 -0500

    logo

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -5,33 +5,20 @@ type ILogoProps = {
 };
 
 const Logo = (props: ILogoProps) => {
-  const size = props.xl ? '44' : '32';
-  const fontStyle = props.xl
-    ? 'font-semibold text-3xl'
-    : 'font-semibold text-xl';
+  // Apply conditional classes for size. Adjust these classes based on your preference.
+  const imageSizeClass = props.xl ? 'w-44 h-44' : 'w-32 h-32'; // Example sizes, adjust as needed
+  const textStyleClass = props.xl ? 'text-3xl' : 'text-xl';
+  const fontWeightClass = 'font-semibold';
 
   return (
-    <span className={`inline-flex items-center text-gray-900 ${fontStyle}`}>
-      <svg
-        className="mr-1 stroke-current text-primary-500"
-        xmlns="http://www.w3.org/2000/svg"
-        width={size}
-        height={size}
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M0 0h24v24H0z" stroke="none" />
-        <rect x="3" y="12" width="6" height="8" rx="1" />
-        <rect x="9" y="8" width="6" height="12" rx="1" />
-        <rect x="15" y="4" width="6" height="16" rx="1" />
-        <path d="M4 20h14" />
-      </svg>
-
+    <div className={`inline-flex items-center text-gray-900 ${fontWeightClass} ${textStyleClass}`}>
+      <img
+        src="/public/assets/images/logo.png" // Adjust the path as necessary
+        alt="Logo"
+        className={`mr-1 ${imageSizeClass}`} // Controls the size of the image
+      />
       {AppConfig.site_name}
-    </span>
+    </div>
   );
 };
 
