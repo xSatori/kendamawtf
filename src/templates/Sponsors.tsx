b9657c99b6eb37973e12f2febf6489e849120215
xSatori <99294685+xSatori@users.noreply.github.com>
2024-02-27 15:17:10 -0500

    Update Sponsors.tsx

--- src/templates/Sponsors.tsx
+++ src/templates/Sponsors.tsx
@@ -11,7 +11,7 @@ const Sponsors = () => (
       <tbody>
         <tr className="h-56">
           <td className="border-2 border-gray-300 p-3">
-            <a href="https://sweetskendamasfoundation.org/" target="_blank">
+            <a href="/" target="_blank">
               <Image
                 src="/assets/images/SKF.png"
                 alt="Sweets Kendamas Foundation"
@@ -22,7 +22,7 @@ const Sponsors = () => (
           </td>
           <td className="border-2 border-gray-300 p-3">
             <a
-              href="https://sentry.io/for/nextjs/?utm_source=github&utm_medium=paid-community&utm_campaign=general-fy25q1-nextjs&utm_content=github-banner-nextjsboilerplate-logo"
+              href="/"
               target="_blank"
             >
               <Image
@@ -35,7 +35,7 @@ const Sponsors = () => (
           </td>
           <td className="border-2 border-gray-300 p-3">
             <a
-              href="https://betterstack.com/?utm_source=github&utm_medium=sponsorship&utm_campaign=next-js-boilerplate"
+              href="/"
               target="_blank"
             >
               <Image
