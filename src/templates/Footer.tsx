40759bb9361c26046c5a48518b3aa50c45a1cba5
xSatori <99294685+xSatori@users.noreply.github.com>
2024-02-27 16:33:47 -0500

    Update Footer.tsx

--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 import { Background } from '../background/Background';
 import { CenteredFooter } from '../footer/CenteredFooter';
 import { Section } from '../layout/Section';
-import { Logo } from './Logo';
+// import { Logo } from './Logo';
 
 const Footer = () => (
   <Background color="bg-gray-100">
     <Section>
       <CenteredFooter
-        logo={<Logo />}
+        // logo={<Logo />}
         iconList={
           <>
             <Link href="/">
