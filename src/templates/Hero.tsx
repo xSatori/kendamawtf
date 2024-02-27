a29919ead2a94eb92e675c79e6248ed90788c79b
xSatori <99294685+xSatori@users.noreply.github.com>
2024-02-27 12:16:27 -0500

    Update Hero.tsx

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,16 +1,16 @@
 import Link from 'next/link';
 
+import { Section } from '@/layout/Section';
 import { Background } from '../background/Background';
 import { Button } from '../button/Button';
 import { HeroOneButton } from '../hero/HeroOneButton';
-import { Section } from '@/layout/Section';
 // import { Section } from '../layout/Section';
 // import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 // import { Logo } from './Logo';
 
 const Hero = () => (
   <Background color="bg-gray-100">
-    {/*<Section yPadding="py-6">
+    {/* <Section yPadding="py-6">
        <NavbarTwoColumns logo={<Logo xl />}>
         <li>
           <Link href="/">Home</Link>
