3c03b8a192e538ea605cab4cd8c9873d8198fda2
xSatori <99294685+xSatori@users.noreply.github.com>
2024-02-27 15:22:38 -0500

    Update Sponsors.tsx

--- src/templates/Sponsors.tsx
+++ src/templates/Sponsors.tsx
@@ -1,49 +1,44 @@
 import Image from 'next/image';
 import React from 'react';
-
 import { Section } from '@/layout/Section';
 
 const Sponsors = () => (
-  <Section 
-    title="Special Thanks" 
+  <Section
+    title="Special Thanks"
     description="This was made possible thanks to these awesome organizations."
-    >
-    <table className="mx-auto border-collapse">
-      <tbody>
-        <tr className="h-56">
-          <td className="border-2 border-gray-300 p-3">
-            <a href="/" target="_blank">
-              <Image
-                src="/assets/images/SKF.png"
-                alt="Sweets Kendamas Foundation"
-                width={260}
-                height={224}
-              />
-            </a>
-          </td>
-          <td className="border-2 border-gray-300 p-3">
-            <a href="/" target="_blank">
-              <Image
-                src="/assets/images/noggles.png"
-                alt="Nouns"
-                width={260}
-                height={224}
-              />
-            </a>
-          </td>
-          <td className="border-2 border-gray-300 p-3">
-            <a href="/" target="_blank">
-              <Image
-                src="/assets/images/sweets.png"
-                alt="Sweets Kendamas"
-                width={260}
-                height={224}
-              />
-            </a>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+  >
+    <div className="flex justify-center items-center flex-wrap">
+      <div className="p-3 border-2 border-gray-300">
+        <a href="/" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/assets/images/SKF.png"
+            alt="Sweets Kendamas Foundation"
+            width={260}
+            height={224}
+          />
+        </a>
+      </div>
+      <div className="p-3 border-2 border-gray-300">
+        <a href="/" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/assets/images/noggles.png"
+            alt="Nouns"
+            width={260}
+            height={224}
+          />
+        </a>
+      </div>
+      <div className="p-3 border-2 border-gray-300">
+        <a href="/" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/assets/images/sweets.png"
+            alt="Sweets Kendamas"
+            width={260}
+            height={224}
+          />
+        </a>
+      </div>
+    </div>
   </Section>
 );
 
